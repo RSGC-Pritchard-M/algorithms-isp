7dbec14b4553b640f384c6a6eb2d1e39436a11fa
RSGC-Pritchard-M <Matthew@MacBook-Pro704-Pritchard.local>
2016-01-21 00:11:51 -0500

    More balls moving and starting to come together

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,21 +9,33 @@
 import Foundation
 
 class Sketch : NSObject {
-
+    
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
     // Declare any properties you need for your sketch below this comment, but before init()
-    var x = 0
-    var s = 1
+    
+    
+    var x1 = 25
+    var s1 = 3
+    var x2 = 110
+    var s2 = -3
+    var x3 = 175
+    var s3 = 3
+    
+//    if (canvas.frameCount > 1000){
+//    var s1 = 0
+//    var s2 = 0
+//    var s3 = 0
+//    }
     var backgroundRed : Bool = false // Boolean to toggle whether background is red
-
+    
     // This runs once, equivalent to setup() in Processing
     override init() {
         
         // Create canvas object – specify size
-        canvas = Canvas(width: 500, height: 300)
+        canvas = Canvas(width: 220, height: 300)
         
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
@@ -36,13 +48,28 @@ class Sketch : NSObject {
         
         //if less than 120 frames dont move
         if (canvas.frameCount > 120 ){
-        // Horizontal position of circle
-        x = x + s
-        
-        // Bounce when hitting wall
-        if (x > canvas.width || x < 0) {
-            s *= -1
+            // Horizontal position of circle
+            x1 = x1 + s1
+            x2 = x2 + s2
+            x3 = x3 + s3
+
+            // Bounce when hitting wall
+            if (x1 > canvas.width || x1 < 0) {
+                s1 *= -1
         }
+        }
+        if (x2 > canvas.width || x2 < 0) {
+            s2 *= -1
+        }
+        
+        if (x3 > canvas.width || x3 < 0) {
+            s3 *= -1
+        }
+        
+        if (canvas.frameCount > 1000){
+            x1 = x1 + 0
+            x2 = x2 + 0
+            x3 = x3 + 0
         }
         // "Clear" the background
         canvas.drawShapesWithBorders = false
@@ -58,34 +85,36 @@ class Sketch : NSObject {
         
         // Draw a circle that moves across the screen
         canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
+        canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: x1, centreY: canvas.height / 2, width: 25, height: 25)
         
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: 50, centreY: canvas.height / 2, width: 25, height: 25)
+        canvas.drawEllipse(centreX: x2, centreY: canvas.height / 2, width: 25, height: 25)
         
         canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: 100, centreY: canvas.height / 2, width: 25, height: 25)
+        canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 100)
+        canvas.drawEllipse(centreX: x3, centreY: canvas.height / 2, width: 25, height: 25)
         
         // Draw some text on the screen
         canvas.textColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100) // white
-        canvas.drawText(message: "Hello, world!")
+        canvas.drawText(message: "Find The Red Ball")
         
         // Draw some more text on the screen
         canvas.textColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 100) // yellow
         canvas.drawText(message: "Current frame: \(canvas.frameCount)", size: 12, x: canvas.width / 16, y: canvas.height - canvas.height / 8)
-
-        // Text with emoji works too
+//        canvas.awnser = false
+//        // Text with emoji works too
+//        if (awnser == true){
         canvas.drawText(message: "😜", size: 48, x: canvas.width - canvas.width / 4, y: canvas.height - canvas.height / 4)
+        }
         
         // Draw a circle where the mouse pointer is
         canvas.drawShapesWithBorders = false
         canvas.drawShapesWithFill = true
         canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
         canvas.drawEllipse(centreX: Int(canvas.mouseX), centreY: Int(canvas.mouseY), width: 5, height: 5)
-
+        
     }
     
     // Respond to the mouseDown event
