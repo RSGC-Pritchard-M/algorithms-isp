612b6394d518ea61893ef4eeaee7a843fa24f48b
RSGC-Pritchard-M <Matthew@MacBook-Pro704-Pritchard.local>
2016-01-24 21:55:51 -0500

    Should work but does not beacause of weird errors

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -2,8 +2,6 @@
 //  Sketch.swift
 //  Animation
 //
-//  Created by Russell Gordon on 2015-12-05.
-//  Copyright © 2015 Royal St. George's College. All rights reserved.
 //
 
 import Foundation
@@ -41,100 +39,104 @@ class Sketch : NSObject {
     
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
-        if(stop == false){
-            
+        
+        
         
         //if less than 120 frames dont move
-        if (canvas.frameCount > 120 ){
+        if (canvas.frameCount < 120){
+            
+            
+        } else if (canvas.frameCount > 120) && (canvas.frameCount < 1000)  {
             // Horizontal position of circle
             x1 = x1 + s1
             x2 = x2 + s2
             x3 = x3 + s3
-
+            
             // Bounce when hitting wall
             if (x1 > canvas.width || x1 < 0) {
                 s1 *= -1
-        }
-        }
-        if (x2 > canvas.width || x2 < 0) {
-            s2 *= -1
-        }
-        
-        if (x3 > canvas.width || x3 < 0) {
-            s3 *= -1
-        }
-        
-        if (canvas.frameCount > 1000){
-            
-            stop = true
-            
-        }
-        // "Clear" the background
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
-        // Decide what color to make the background
-        if (backgroundRed == true) {
-            canvas.fillColor = Color(hue: 0, saturation: 80, brightness: 90, alpha: 100)
-        } else {
-            canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
-        }
-        canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
-        
-        
-        // Draw a circle that moves across the screen
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x1, centreY: canvas.height / 2, width: 25, height: 25)
-        
-        if (canvas.frameCount < 120){
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue:0, saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x2, centreY: canvas.height / 2, width: 25, height: 25)
-        }
-        if (canvas.frameCount > 120){
-            canvas.drawShapesWithBorders = false
-            canvas.fillColor = Color(hue:240, saturation: 80, brightness: 90, alpha: 100)
-            canvas.drawEllipse(centreX: x2, centreY: canvas.height / 2, width: 25, height: 25)
-        }
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x3, centreY: canvas.height / 2, width: 25, height: 25)
-        
-        // Draw some text on the screen
-        canvas.textColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100) // white
-        canvas.drawText(message: "Find The Red Ball")
-        
-        // Draw some more text on the screen
-        canvas.textColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 100) // yellow
-        canvas.drawText(message: "Current frame: \(canvas.frameCount)", size: 12, x: canvas.width / 16, y: canvas.height - canvas.height / 8)
-            
-            
-        // Draw a circle where the mouse pointer is
-        canvas.drawShapesWithBorders = false
-        canvas.drawShapesWithFill = true
-        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
-        canvas.drawEllipse(centreX: Int(canvas.mouseX), centreY: Int(canvas.mouseY), width: 5, height: 5)
-        } else {
-            func mouseDown() {
-                if (canvas.mouseX >  ){
-            
-            if (awnser ==  false){
-                canvas.drawText(message: "❌", size: 48, x: canvas.width - canvas.width / 4, y: canvas.height - canvas.height / 4)
-            }
-            if (awnser ==  true){
-                canvas.drawText(message: "✅", size: 48, x: canvas.width - canvas.width / 4, y: canvas.height - canvas.height / 4)
             }
             
+        } else { // 1000 or greater (frameCount)
+            
+            if mouseDown(x2-12 > canvas.mouseX > x2+12 && 88 < canvas.mouseY < 112 {
+            
+            awnser = true
+           
+            }
+        }
+        
+                if (x2 > canvas.width || x2 < 0) {
+                    s2 *= -1
                 }
-            }
-        }
-            
+        
+                if (x3 > canvas.width || x3 < 0) {
+                    s3 *= -1
+                }
         
         
-    }
+                // "Clear" the background
+                canvas.drawShapesWithBorders = false
+                canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
+                // Decide what color to make the background
+                if (backgroundRed == true) {
+                    canvas.fillColor = Color(hue: 0, saturation: 80, brightness: 90, alpha: 100)
+                } else {
+                    canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
+                }
+                canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
+        
+        
+                // Draw a circle that moves across the screen
+                canvas.drawShapesWithBorders = false
+                canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 100)
+                canvas.drawEllipse(centreX: x1, centreY: canvas.height / 2, width: 24, height: 24)
+        
+                if (canvas.frameCount < 120){
+                    canvas.drawShapesWithBorders = false
+                    canvas.fillColor = Color(hue:0, saturation: 80, brightness: 90, alpha: 100)
+                    canvas.drawEllipse(centreX: x2, centreY: canvas.height / 2, width: 24, height: 24)
+                }
+                if (canvas.frameCount > 120){
+                    canvas.drawShapesWithBorders = false
+                    canvas.fillColor = Color(hue:240, saturation: 80, brightness: 90, alpha: 100)
+                    canvas.drawEllipse(centreX: x2, centreY: canvas.height / 2, width: 24, height: 24)
+                }
+                canvas.drawShapesWithBorders = false
+                canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 100)
+                canvas.drawEllipse(centreX: x3, centreY: canvas.height / 2, width: 24, height: 24)
+        
+                // Draw some text on the screen
+                canvas.textColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100) // white
+                canvas.drawText(message: "  Find The Red Ball")
+        
+                // Draw some more text on the screen
+                canvas.textColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 100) // yellow
+                canvas.drawText(message: "Current frame: \(canvas.frameCount)", size: 12, x: canvas.width / 16, y: canvas.height - canvas.height / 8)
+        
+        
+                // Draw a circle where the mouse pointer is
+                canvas.drawShapesWithBorders = false
+                canvas.drawShapesWithFill = true
+                canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
+                canvas.drawEllipse(centreX: Int(canvas.mouseX), centreY: Int(canvas.mouseY), width: 5, height: 5)
+        
+                if (awnser == false) {
+                    canvas.drawText(message: "❌", size: 48, x: canvas.width - canvas.width / 4, y: canvas.height - canvas.height / 4)
+                }
+        
+                if (awnser ==  true){
+                    canvas.drawText(message: "✅", size: 48, x: canvas.width - canvas.width / 4, y: canvas.height - canvas.height / 4)
+                }
+        
+        
+    } // END of draw() func
+    
+    
     
     // Respond to the mouseDown event
     func mouseDown() {
+        
         
         // When the mouse is pressed, change the background color that will be used.
         if backgroundRed == true {
@@ -142,7 +144,7 @@ class Sketch : NSObject {
         } else {
             backgroundRed = true
         }
-        
     }
     
 }
+
